dcbd0d6c716d2a9524d0581919e8c6b83cc2017e
Guntur <gugunguntur99@gmail.com>
2024-08-16 09:45:04 +0700

    add initial face recognition at profile

---
+++ upload-models-to-canister.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+dfx canister call pickme_face_recognition clear_face_detection_model_bytes
+dfx canister call pickme_face_recognition clear_face_recognition_model_bytes
+ic-file-uploader pickme_face_recognition append_face_detection_model_bytes version-RFB-320.onnx
+ic-file-uploader pickme_face_recognition append_face_recognition_model_bytes face-recognition.onnx
+dfx canister call pickme_face_recognition setup_models
